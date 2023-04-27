8ee8bd4cbb0abe4e658f4cf0435b6a37df819ed9
jdormannn <89854560+jdormannn@users.noreply.github.com>
2023-04-27 17:13:42 -0400

    Initial Commit

---
+++ LocoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocoApp
+//
+//  Created by Jordan Dorman on 4/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
