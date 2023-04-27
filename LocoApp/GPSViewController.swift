c3fb50c2e9486d6cbc10c262e7a71738e4ab31e4
jdormannn <89854560+jdormannn@users.noreply.github.com>
2023-04-27 18:52:27 -0400

    DONE

--- LocoApp/GPSViewController.swift
+++ LocoApp/GPSViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import CoreLocation
-
-class GPSViewController: UIViewController, CLLocationManagerDelegate {
+import WebKit
+import MessageUI
+class GPSViewController: UIViewController, CLLocationManagerDelegate, MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        
+    }
     
     
     
@@ -17,7 +21,8 @@ class GPSViewController: UIViewController, CLLocationManagerDelegate {
     
     
     
-    @IBOutlet var distanceLabel: UILabel!
+    
+    @IBOutlet var distanceLabel: UILabel?
     
     
     let locMan: CLLocationManager = CLLocationManager()
@@ -38,12 +43,12 @@ class GPSViewController: UIViewController, CLLocationManagerDelegate {
             let miles: Double = (delta * 0.000621371) + 0.5
             if miles < 3 {
                 locMan.stopUpdatingLocation()
-                distanceLabel.text = " Enjoy\nNewJersey!"
+                distanceLabel?.text = " Enjoy\nNewJersey!"
                 
             }else {
                 let commaDelimited: NumberFormatter = NumberFormatter()
                 commaDelimited.numberStyle = NumberFormatter.Style.decimal
-                distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to NewJersey"
+                distanceLabel?.text=commaDelimited.string(from: NSNumber(value: miles))!+" miles to NewJersey"
             }
             
             
@@ -57,47 +62,71 @@ class GPSViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     
+    @IBOutlet weak var webView: WKWebView?
     
     
+    @IBAction func openSite(_ sender: Any) {
+        if let url = URL(string: "https://visitnj.org/?utm_source=google&utm_medium=cpc&utm_campaign=Brand&gclid=CjwKCAjwuqiiBhBtEiwATgvixJiWzmh4oSJlkELeFJK3Rk_9V8MHHy5fOmxoycDI9HnTXdhaTRIMyRoCCPkQAvD_BwE"){
+            UIApplication.shared.open(url, options:[:])
+        }
+    }
+        
+    @IBAction func sendMessage(_ sender: Any) {
+        
+        let SMS = MFMessageComposeViewController()
+                        SMS.messageComposeDelegate = self
+                        
+                        SMS.recipients = ["2016732243"]
+                        SMS.body = "Hello message!"
+                        
+                        if MFMessageComposeViewController.canSendText() {
+                            self.present(SMS, animated:true, completion:nil)
+                        }else{
+                            print("Cant send messages.")
+                        }
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        locMan.delegate = self
-        locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-        locMan.distanceFilter = 1609;
-        locMan.requestWhenInUseAuthorization()
-        locMan.startUpdatingLocation()
-        startLocation = nil
         
         
         
         
         
         
-        
-        
-        
-        
-        /*
-         // MARK: - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-         }
-         */
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+            locMan.delegate = self
+            locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locMan.distanceFilter = 1609;
+            locMan.requestWhenInUseAuthorization()
+            locMan.startUpdatingLocation()
+            startLocation = nil
+            
+            //load website
+                    let myURL = URL(string: "https://visitnj.org/?utm_source=google&utm_medium=cpc&utm_campaign=Brand&gclid=CjwKCAjwuqiiBhBtEiwATgvixJiWzmh4oSJlkELeFJK3Rk_9V8MHHy5fOmxoycDI9HnTXdhaTRIMyRoCCPkQAvD_BwE")
+                    let myRequest = URLRequest(url: myURL!)
+                   webView?.load(myRequest)
+            
+            
+            
+            
+            
+            
+            
+            
+            /*
+             // MARK: - Navigation
+             
+             // In a storyboard-based application, you will often want to do a little preparation before navigation
+             override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+             }
+             */
+            
+        }
         
     }
     
-}
 
